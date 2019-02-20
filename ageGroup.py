f1a0a33442d774e439163fab93315887270988b4
Panto1212 <patienceankundat@gmail.com>
2019-02-20 09:58:47 +0300

    adjust the loop

--- ageGroup.py
+++ ageGroup.py
@@ -3,7 +3,11 @@ year= int(input())
 
 age = 2019-year
 
-if age<18: print("You are a minor")
-elif age>=18 and age<=36 : print("You are a youth")
-elif age>36 : print("You are an elder")
-else : print("Invalid entry")
+if age<18: 
+    print("You are a minor")
+elif age>=18 and age<=36 :
+    print("You are a youth")
+elif age>36 : 
+    print("You are an elder")
+else : 
+    print("Invalid entry")
